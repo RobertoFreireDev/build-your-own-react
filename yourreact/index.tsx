9b93cbe27096b43f685e72eadd1dd62b6f776a27
roberto freire <robertofreireferrazpassos@gmail.com>
2026-02-01 12:22:58 -0300

    adjust code to test with real react

--- yourreact/index.tsx
+++ yourreact/index.tsx
@@ -1,11 +1,22 @@
-import { React, createRoot } from "./customreactlib/react";
+import { React } from "./customreactlib/react";
+import { createRoot } from "./customreactlib/react-dom";
 
-const App = () => <div className="customClass">
-    <h1>Hello, person!</h1>
-    <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae nemo tempore sint nihil, cumque incidunt. Aspernatur eaque molestiae praesentium dolorem officiis officia expedita mollitia voluptas aliquid inventore, quam sed quas.
-    </p>
-</div>;
+const count = 0;
+const setCount = (c) => { c(count);}
+
+const App = () => {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Custom React</h1>
+      <p>Count: {count}</p>
+      <button onclick={() => setCount(c => c + 1)}>
+        Increment
+      </button>
+    </div>
+  )
+}
 
 const root = createRoot(document.getElementById("root"))
 root.render(<App/>);
